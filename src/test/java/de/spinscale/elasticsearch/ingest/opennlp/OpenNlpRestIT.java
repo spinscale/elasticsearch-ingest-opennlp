bd28782086f53747417c221d301cb97f425de3b4
Alexander Reelsen <alexander@reelsen.net>
2017-07-07 10:31:06 +0200

    Dependencies: Upgrade to Elasticsearch 5.5.0

--- src/test/java/de/spinscale/elasticsearch/ingest/opennlp/OpenNlpRestIT.java
+++ src/test/java/de/spinscale/elasticsearch/ingest/opennlp/OpenNlpRestIT.java
@@ -22,8 +22,6 @@ import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
 
-import java.io.IOException;
-
 public class OpenNlpRestIT extends ESClientYamlSuiteTestCase {
 
     public OpenNlpRestIT(@Name("yaml") ClientYamlTestCandidate testCandidate) {
@@ -31,7 +29,7 @@ public class OpenNlpRestIT extends ESClientYamlSuiteTestCase {
     }
 
     @ParametersFactory
-    public static Iterable<Object[]> parameters() throws IOException {
+    public static Iterable<Object[]> parameters() throws Exception {
         return ESClientYamlSuiteTestCase.createParameters();
     }
 }
